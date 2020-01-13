e4209945ea62e3eb9b31e525bc37b72ecebe425a
habib-m-ahmed <45317296+habib-m-ahmed@users.noreply.github.com>
2020-01-12 21:23:25 -0500

    Update StoreStatistics.java

--- StoreStatistics.java
+++ StoreStatistics.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
+
 public class StoreStatistics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		
-File f = new File ("Project01Data.txt");
+		File f = new File ("WorkingExampleDataFile.txt");
 		
 		// A BufferedReader is an object that lets you read a stream of Text Data
 
@@ -42,52 +42,23 @@ File f = new File ("Project01Data.txt");
 
 				// Used to read the line of text / (String) from the file
 
-				String sStoreNum [] [] = {
+				String sProvs [] [] = {
 						{"  ", "TOTAL"},
-						{"101", "St. John's, Newfoundland"},
-						{"102", "Halifax, Nova Scotia"},
-						{"103", "Fredericton, New Brunswick"},
-						{"201", "Quebec City, Quebec"},
-						{"202", "Montreal, Quebec"},
-						{"301", "Ottawa, Ontario"},
-						{"302", "Toronto, Ontario"},
-						{"303", "Mississauga, Ontario"},
-						{"304", "Hamilton, Ontario"},
-						{"305", "Kitchener, Ontario"},
-						{"401", "Winnipeg, Manitoba"},
-						{"402", "Regina, Saskatchewan"},
-						{"403", "Calgary, Alberta"},
-						{"404", "Edmonton, Alberta"},
-						{"405", "Vancouver, British Columbia"},
-						{"??", "Unknown"},	
-						
-						
+						{"BC", "British Columbia"},
+						{"AB", "Alberta"},
+						{"SK", "Saskatchewan"},
+						{"MB", "Manitoba"},
+						{"ON", "Ontario"},
+						{"QC", "Quebec"},
+						{"NB", "New Bruinswick"},
+						{"NS", "Nova Scotia"},
+						{"PE", "Prince Edward Island"},
+						{"NL", "Newfoundland & Labrador"},
+						{"??", "Unknown"},		
 				};
-				
-				String sProductList [] [] = {
-						{"  ", "TOTAL"},
-						{"PR121", "Metal Pipe"},
-						{"PR122", "Smooth Wall Steel Pipe"},
-						{"PR123", "Corrugated Metal Pipe"},
-						{"PR124", "S.P.C Metal Pipe"},
-						{"PR231", "Tunnel Liner Plate"},
-						{"PR331", "Concrete Arch Systems"},
-						{"PR441", "Gates"},
-						{"PR511", "Polyethylene Pipe"},
-						{"PR512", "Polyvinyl Chloride Pipe"},
-						{"PR513", "Reinforced Concrete Pipe"},
-						{"PR661", "Beaver Control"},
-						{"PR701", "End Treatments"},
-						{"PR811", "Culvert Liner"},
-						{"??", "Unknown"},	
-						
-				};
-						
-					
 				
 				double dSalesAmt = 0; //Temp variable to hold the sales amount
-				
-				double [] [] [] dTableOfData = new double [17] [15] [14];
+				double [] [] dTableOfData = new double [12] [14];
 				//DTABLEOFDATA will be used to hold the sales totals
 				//The rows represent the provinces
 				//		Row 0 - Column Totals
@@ -102,16 +73,15 @@ File f = new File ("Project01Data.txt");
 				//
 				
 				int iLinesRead = 0;			//  ILINESREAD is used to track the number of lines read from the file
-				int iStore = 0;				//  iStore is used to position the row (store) in DATATABLE
-				int iProduct = 0;			//  iProduct is used to position the column (product) in DATATABLE
-				int iMonth = 0;				//	iMonth is used to position the month in DATATABLE
+				int iRow = 0;				//  IROW is used to position the row in DATATABLE
+				int iCol = 0;				//  ICOl is used to position the column in DATATABLE
 				
 				String sValue = "";
 				String [] data;
 				//DATA is used to split the information on a single line
 			
 				boolean done = false;
-				// Use to indicate when there's no more data in the file 
+				// Use to indicate when there's no more data in the file
 
 	do {
 
@@ -153,59 +123,39 @@ File f = new File ("Project01Data.txt");
 					
 					//////////////////////////////////////////////
 					
-					
 					//System.out.println("Line: "+line);
 					data = line.split(",");
-					if (data.length==4)
-					{
-					
 					//System.out.println("Data0: "+data[0]);
 					//System.out.println("Data1: "+data[1]);
 					//System.out.println("Data2: "+data[2]);
-					//System.out.println("Data3: "+data[3]);
 					
-					
-					//Search through the store code-
+					//Search through the province code-
 					//array for a match. Use the match-
 					//location as the array row #.
 					
-						for (iProduct = 1; iProduct<=13; iProduct++)
-						{
-							if (data[2].equals(sProductList[iProduct][0])) break;
-						}
-						
-						
-						
-					for (iStore = 1; iStore<=15; iStore++)
+					for (iRow = 1; iRow<=10; iRow++)
 					{
-						if (data[0].equals(sStoreNum[iStore][0])) break;
+						if (data[0].equals(sProvs[iRow][0])) break;
 					}
 					
 					//Extract the month number out of the date
 					
-					iMonth = Integer.parseInt(data[1]);
-					iMonth = (iMonth / 100) % 100;
-					if ((iMonth < 1) | (iMonth>12)) iMonth = 13;
+					iCol = Integer.parseInt(data[1]);
+					iCol = (iCol / 100) % 100;
+					if ((iCol < 1) | (iCol>12)) iCol = 13;
 					
 					//Convert the Sales Amount from a-
 					//string into a numeric value.
 					
-					dSalesAmt = Double.parseDouble(data[3]);
+					dSalesAmt = Double.parseDouble(data[2]);
 					
 					//Populate the table with the sales amount 
 					
-					dTableOfData [iStore][iProduct][iMonth] += dSalesAmt;	//Prov & month sales
-					dTableOfData [iStore][iProduct][     0] += dSalesAmt;	//Grand total
-					dTableOfData [iStore][       0][iMonth] += dSalesAmt;	//Prov total for the year
-					dTableOfData [     0][       0][     0] += dSalesAmt;	//Month total for all provs
+					dTableOfData [iRow][iCol] += dSalesAmt;		//Prov & month sales
+					dTableOfData [   0][   0] += dSalesAmt;		//Grand total
+					dTableOfData [iRow][   0] += dSalesAmt;		//Prov total for the year
+					dTableOfData [   0][iCol] += dSalesAmt;		//Month total for all provs
 					
-					dTableOfData [     0][iProduct][iMonth] += dSalesAmt;	
-					dTableOfData [     0][iProduct][     0] += dSalesAmt;	
-					dTableOfData [     0][       0][iMonth] += dSalesAmt;	
-					dTableOfData [     0][       0][     0] += dSalesAmt;	
-					
-					
-					}
 				}
 	} while (!done);
 	
@@ -213,111 +163,77 @@ File f = new File ("Project01Data.txt");
 	
 	//Now print the report!
 	
-	// loop through each store 
-
-	for (iStore = 0; iStore<=16; iStore++)
-	{
-		sValue = padRString(sStoreNum[iStore][1],25);
-		
-		
-		System.out.print(sValue);
-	
 	NumberFormat nf = new DecimalFormat("##,###,###.00");
 	NumberFormat pf = new DecimalFormat("###.0");
 	int iLen = 13;
 	
 	System.out.println ("                                                     "	//53 spaces
-					  + "2016 SALES for ABC ROAD SYSTEMS LTD..");
-	System.out.println ("PRODUCTS                 JANUARY      FEBRARY      MARCH        APRIL"
-					  + "        MAY          JUNE         JULY         AUGUST       SEPTEMBER    OCTOBER"
-					  + "      NOVEMBER     DECEMBER    UNKNOWN MONTH YEAR TOTAL    %");
-	System.out.println ("------------------------ ----------   ---------   ----------   ----------"
-					  + "    --------     ----------   ----------   ----------   ----------   ---------"
-	                  + "    ----------   ----------  ------------- ----------   -----");
+					  + "2016    SALES    for    ROAD    SYSTEMS    LTD.");
+	System.out.println ("PROVINCE                      JANUARY      FEBRARY      MARCH      APRIL"
+					  + "         MAY          JUNE         JULY       AUGUST     SEPTEMBER   OCTOBER"
+					  + "     NOVEMBER     DECEMBER    YEAR TOTAL    %");
+	System.out.println ("------------------------     ----------   ---------   ----------   --------"
+					  + "   ----------   ----------   ----------   ----------  ----------  ----------"
+	                  + "   ----------   ----------   ----------  -----");
 	
-	for (iProduct = 1; iProduct<=14; iProduct++)
+	for (iRow = 1; iRow<=11; iRow++)
 	{
-		//Print full product name
-		sValue = padRString(sProductList[iProduct][1],25);
-		
+		//Print full province name
+		sValue = sProvs[iRow][1];
+		while (sValue.length() < 25) {sValue += " ";}
 		System.out.print(sValue);
 		
-		//Loop through the columns and print the months
-		for (iMonth = 1; iMonth<=13; iMonth++) 
+		//Loop through the columns and print the monthly totals
+		for (iCol = 1; iCol<=12; iCol++) 
 			{
-				sValue = padRString(nf.format(dTableOfData [iStore][iProduct][iMonth]),iLen);
+				sValue = nf.format(dTableOfData [iRow][iCol]);
+				while (sValue.length() < iLen) {sValue = " "+sValue;}
 				System.out.print(sValue);
 				
 			}
 			
-			//Print total for each product (Row)
+			//Print total for the province (Row)
 			
-			sValue = padRString(nf.format(dTableOfData [iStore][iProduct][0]),iLen);//
+			sValue = pf.format(dTableOfData [iRow][0]);
+			while (sValue.length() < iLen) {sValue = " "+sValue;}
 			System.out.print(sValue);
 			
 			//Print percentage for the province (Row)
 			
-			sValue = padLString(pf.format(100.0 * dTableOfData [iStore][iProduct][0] / dTableOfData [iStore][0][0]),3);
+			sValue = pf.format(100.0 * dTableOfData [iRow][0] / dTableOfData [0][0]);
+			while (sValue.length() < 5) {sValue = " "+sValue;}
 			System.out.println(sValue+"%");
 			
 		}
-	
+		
 		//Print column totals
 		
-		System.out.println ("=======================  ==========   ==========   ==========   =========="
-				          + "   ==========   ==========   ==========   ==========   ==========   =========="
-				          + "   ==========   ==========   ==========   ==========     =====");
+		System.out.println ("=======================      ==========    ==========     ==========     =========="
+				          + "   ==========   ==========    ==========    ==========     ==========     =========="
+				          + "   ==========   ==========    ==========  =====");
 		//Loop through the columns and print the monthly totals
 		System.out.print("                         ");
-		for (iProduct = 1; iProduct<=12; iProduct++)
+		iRow=0;
+		for (iCol = 1; iCol<=12; iCol++)
 		{
-			sValue = padRString(nf.format(dTableOfData [iStore][iProduct][0]),iLen);//
+			sValue = nf.format(dTableOfData [iRow][iCol]);
+			while (sValue.length() < iLen) {sValue = " "+sValue;}
 			System.out.print(sValue);
 			
 		}
 		
-		//Print the yearly totals
-		
-		sValue = padRString(nf.format(dTableOfData [iStore][iProduct][0]),iLen);//
-		System.out.print(sValue);
-		
-		
 		//Print total for the province (Row)
 		
-		sValue = padLString(pf.format(dTableOfData [iStore][0][0]),5);//
+		sValue = pf.format(dTableOfData [iRow][0]);
+		while (sValue.length() <iLen) {sValue = " "+sValue;}
 		System.out.print(sValue);
 		
-		//Print percentage total for the province (Row)
+		//Print percentage for the province (Row)
 		
-		sValue = padLString(pf.format(100.0 * dTableOfData [iStore][iProduct][0] / dTableOfData [iStore][0][0]),5);
+		sValue = pf.format(100.0 * dTableOfData [iRow][0] / dTableOfData [0][0]);
+		while (sValue.length() < 5) {sValue = " "+sValue;}
 		System.out.println(sValue+"%");
 		
 			
-	}	
-	}
-
-	public static String padRString (String sName, int iWidth)
-	{
-		
-		while (sName.length()<iWidth)
-		{
-			sName+=" ";
 		}
-		
-		return sName;
-		
 	}
-	
-	
-	public static String padLString (String sName, int iWidth)
-	{
-		while (sName.length()<iWidth)
-		{
-			sName=" "+sName;
-
-		}
-		
-		return sName;
-	}
-	
-}
